e4ce3e98ec5daa1b663b2d04d64990b635c6029b
IYATT-yx <iyatt@iyatt.com>
2024-05-03 09:53:00 +0800

    2024.5.3 refactor, feat 使用新的数据库连接验证, 添加数据库表结构自动同步(创建)

--- accounts1/src/index.js
+++ accounts1/src/index.js
@@ -2,18 +2,22 @@
 // 执行入口
 import app from "./app.js";
 import config from "./config.js";
-import getDb from "./database/db.js";
+import { Accounts, authenticateDatabase } from "./models/accountsModel.js";
 
-try {
-    await getDb().authenticate();
-    console.log('成功建立连接。');
-} catch (error) {
-    console.error('无法连接到数据库：', error);
-    process.exit(1);
-}
+authenticateDatabase()
+.then(() => {
+    console.log('数据库连接成功');
+})
+.catch((err) => {
+    console.error('数据库连接失败：', err);
+    process.exit(1)
+})
+
+// 同步 Accounts 模型，表结构不存在会自动创建
+Accounts.sync()
 
 app.listen(config.server.port, config.server.host, () => {
-    console.log(`Server is running on http://${config.server.host}:${config.server.port}/accounts`);
+    console.log(`Server is running on http://${config.server.host}:${config.server.port}${config.baseUrl}`);
 });
 
 process.on('uncaughtException', (err) => {
